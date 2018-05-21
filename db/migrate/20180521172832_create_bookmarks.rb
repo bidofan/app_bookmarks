c11156e5224f68ad5cef64469538e968862bdb0f
vasuok <sbidochka@mail.ru>
2018-05-21 20:29:22 +0300

    generated  bookmark model

---
+++ db/migrate/20180521172832_create_bookmarks.rb
@@ -0,0 +1,11 @@
+class CreateBookmarks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :bookmarks do |t|
+      t.string :url
+      t.string :screenshot_url
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
