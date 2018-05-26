5d2e5f79665de8b6f5d015eb51e525f53cd35f5e
vasuok <sbidochka@mail.ru>
2018-05-26 21:25:05 +0300

    added test

--- spec/models/bookmark_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe Bookmark, :type =>   :model do
-  it 'is valid with valid attributes' do
-    bookmark = Bookmark.new(id: 1, url: "http://google.com", screenshot_url: "someurl.com", user_id: 2)
-    expect(bookmark).to be_valid
-  end
-  it 'is not valid without a url'
-end
