6ca7fb53ea1431902f9ed751a7861f170dcccbe8
vasuok <sbidochka@mail.ru>
2018-05-27 21:51:38 +0300

    something

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '1027365290748195', '6c16a2e2fcaf0cfbefd5e58011a10c8c', :scope => 'email,user_birthday,read_stream'
+  provider :facebook, '1027365290748195', '6c16a2e2fcaf0cfbefd5e58011a10c8c'
 end
